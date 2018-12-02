3d626fbf5fe1c648f66d81d82764f7db93d7780d
Matthew D <matthewdawson@outlook.com>
2018-12-02 13:59:15 +1300

    Add Cheatsheet model and controller

---
+++ app/models/cheatsheet.rb
@@ -0,0 +1,2 @@
+class Cheatsheet < ApplicationRecord
+end
---
+++ db/migrate/20181202005846_create_cheatsheets.rb
@@ -0,0 +1,8 @@
+class CreateCheatsheets < ActiveRecord::Migration[5.2]
+  def change
+    create_table :cheatsheets do |t|
+
+      t.timestamps
+    end
+  end
+end
