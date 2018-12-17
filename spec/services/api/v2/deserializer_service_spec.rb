995ee0ca8d9e2dadcc961d8dceb39ea4d7183c9c
Matthew D <matthewdawson@outlook.com>
2018-12-17 21:43:50 +1300

    Abstract processed_json out to let

--- spec/services/api/v2/deserializer_service_spec.rb
+++ spec/services/api/v2/deserializer_service_spec.rb
@@ -29,7 +29,7 @@ RSpec.describe 'deserializer_service' do
   }
 
   let(:deserialized_json){
-    Api::V2::DeserializerService.new(request).process_json
+    Api::V2::DeserializerService.new(request)
   }
 
   it 'has valid data' do
@@ -37,27 +37,27 @@ RSpec.describe 'deserializer_service' do
   end
 
   it 'processes the title metadata' do
-    deserialized_json = Api::V2::DeserializerService.new(request).process_json
-    expect(deserialized_json.title).to eq('Vim')
+    processed_json = deserialized_json.process_json
+    expect(processed_json.title).to eq('Vim')
   end
 
   it 'processes the description metadata' do
-    deserialized_json = Api::V2::DeserializerService.new(request).process_json
-    expect(deserialized_json.description).to eq('Imported cheatsheet')
+    processed_json = deserialized_json.process_json
+    expect(processed_json.description).to eq('Imported cheatsheet')
   end
 
   it 'validates the json' do
-    valid_json = Api::V2::DeserializerService.new(request).validate_json!(request)
-    expect(valid_json).to be true
+    validated_json = deserialized_json.validate_json!(request)
+    expect(validated_json).to be true
   end
 
   it 'does not validate invalid json' do
-    invalid_json = Api::V2::DeserializerService.new(invalid_request)
-    expect(invalid_json.validate_json!(invalid_request)).to eq("The property '#/' did not contain a required property of 'metadata'")
+    invalidated_json = Api::V2::DeserializerService.new(invalid_request)
+    expect(invalidated_json.validate_json!(invalid_request)).to eq("The property '#/' did not contain a required property of 'metadata'")
   end
 
   it 'returns a cheatsheet' do
-    deserialized_json = Api::V2::DeserializerService.new(request).process_json
-    expect(deserialized_json.class).to eq(Cheatsheet)
+    processed_json = deserialized_json.process_json 
+    expect(processed_json.class).to eq(Cheatsheet)
   end
 end
