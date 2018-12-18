c79233c922d07f6114c7b26d5c06b672ce4ecec6
Matthew D <matthewdawson@outlook.com>
2018-12-18 21:03:32 +1300

    Add validations to user and email

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,8 @@
 class User < ApplicationRecord
+  EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+
+  validates :name, presence: true, length: { maximum: 25 }
+  validates :email, presence: true, length: { maximum: 255 },
+    format: { with: EMAIL_REGEX },
+    uniqueness: { case_sensitive: false }
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,33 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+    let(:user) {
+      User.new(:name => 'Testee', :email => 'test@user.com')
+    }
+
+  it 'should be valid' do
+    expect(user.valid?).to be true
+  end
+
+  it 'should have a name' do
+    user.name = ''
+    expect(user.valid?).to be false
+  end
+
+  it 'should have an email' do
+    user.email = ''
+    expect(user.valid?).to be false
+  end
+
+  it 'should be a valid email' do
+    user.email = 'test_email'
+    expect(user.valid?).to be false
+  end
+
+  it 'should have a unique email' do
+    duplicate_user = user.dup
+    user.save
+    expect(duplicate_user.valid?).to be false
+  end
 end
