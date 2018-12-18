67b1b8fa7735fbe2f9104267c40e1e3da5afcc80
Matthew D <matthewdawson@outlook.com>
2018-12-18 21:24:24 +1300

    Remove factories - Fix broken production

--- app/spec/factories.rb
+++
@@ -1,7 +0,0 @@
-FactoryBot.define do
-
-  factory :request do
-    metadata {"title": "Vim","description": "Imported cheatsheet"}
-    payload [{"category": "Global","commands": [{"term": ":help keyword","description": ":help keyword - open help for keyword"}, {"term": ":saveas file","description": ":saveas file - save file as"}]}]
-  end
-end
