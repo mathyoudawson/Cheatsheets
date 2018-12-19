09c2253f116206fe65884e2d93049fafa2610788
Matthew D <matthewdawson@outlook.com>
2018-12-20 12:20:12 +1300

    Invalid user signup path

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   get 'users/new'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
+  resources :users
+
   resources :cheatsheets do
     resources :cheats
   end
