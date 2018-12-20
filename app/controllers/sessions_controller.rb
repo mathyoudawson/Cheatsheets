c412d806f62d86b4f3bce7e1d04f12f3e0a615e8
Matthew D <matthewdawson@outlook.com>
2018-12-20 15:44:28 +1300

    Handle logging in

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,17 @@
 class SessionsController < ApplicationController
   def new
   end
+
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      flash.now[:success] = 'Successfully logged in!'
+    else
+      flash.now[:danger] = 'Invalid username or password'
+      render 'new'
+    end
+  end
+
+  def destroy
+  end
 end
