7504172db4af81c4536e061d96939ce9ba7b598b
Matthew D <matthewdawson@outlook.com>
2018-12-24 16:13:08 +1300

    Generate migrations for user admin boolean

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_12_20_200716) do
+ActiveRecord::Schema.define(version: 2018_12_24_031106) do
 
   create_table "cheats", force: :cascade do |t|
     t.string "term"
@@ -38,6 +38,7 @@ ActiveRecord::Schema.define(version: 2018_12_20_200716) do
     t.datetime "updated_at", null: false
     t.string "password_digest"
     t.string "remember_digest"
+    t.boolean "admin", default: false
   end
 
 end
