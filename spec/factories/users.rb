c0b30b2b4141dc7b12f5705483c91f782c1cb29d
Matthew D <matthewdawson@outlook.com>
2018-12-25 22:34:28 +1300

    Add edit user spec

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,12 +1,5 @@
 FactoryBot.define do
   factory :user do
-    name 'John'
-    email 'john@doe.com'
-    password 'foobar'
-    password_confirmation 'foobar'
-  end
-
-  factory :random_user do
     password = Faker::Internet.password
     name { Faker::Name.name }
     email  { Faker::Internet.email }
---
+++ spec/features/view_users_spec.rb
@@ -0,0 +1,45 @@
+require 'rails_helper'
+
+describe 'users', type: :feature do
+    let(:user) { create(:user) }
+    let(:admin_user) { create(:user, admin: true) }
+  describe "viewing users", type: :feature do
+
+    it 'views all users' do
+      page.set_rack_session(user_id: user.id)
+
+      visit '/users'
+
+      expect(page).to have_content 'All users'
+    end
+
+    it 'as an admin' do
+      create(:user)
+      page.set_rack_session(user_id: admin_user.id)
+
+      visit '/users'
+
+      expect(page).to have_content 'All users'
+      expect(page).to have_content 'delete'
+    end
+  end
+
+  describe "deleting users", type: :feature do
+    it 'as an admin' do
+      create(:user)
+      page.set_rack_session(user_id: admin_user.id)
+
+      visit '/users'
+      click_link 'delete'
+
+      expect(page).to have_content 'User deleted'
+    end
+    it 'admin can not delete themselves' do
+page.set_rack_session(user_id: admin_user.id)
+
+      visit '/users'
+
+      expect(page).to_not have_content 'delete'
+    end
+  end
+end
