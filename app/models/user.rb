90376427ad66df37b14ddda460d72557177bbb42
Matthew D <matthewdawson@outlook.com>
2018-12-27 10:47:34 +1300

    Create activation digest before creating a user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,8 @@
 class User < ApplicationRecord
-  attr_accessor :remember_token
+  attr_accessor :remember_token, :activation_token
 
-  before_save { self.email = email.downcase }
+  before_save :downcase_email
+  before_create :create_activation_digest
 
   EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 
@@ -41,5 +42,16 @@ class User < ApplicationRecord
 
   def forget
     update_attribute(:remember_digest, nil)
+  end
+
+  private
+
+  def downcase_email
+    self.email = email.downcase
+  end
+
+  def create_activation_digest
+    self.activation_token = User.new_token
+    self.activation_digest = User.digest(activation_token)
   end
 end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -5,5 +5,7 @@ FactoryBot.define do
     email  { Faker::Internet.email }
     password { password }
     password_confirmation { password }
+    activated { true }
+    activated_at { Time.zone.now }
   end
 end
