a1f296b25780f358f317d385f2862a60c0071007
Matthew D <matthewdawson@outlook.com>
2018-12-27 22:09:47 +1300

    Generate controller and routes for password reset

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'password_resets/new'
+  get 'password_resets/edit'
   root 'cheatsheets#index'
   get    '/signup', to: 'users#new'
   get    '/login',  to: 'sessions#new'
@@ -8,6 +10,7 @@ Rails.application.routes.draw do
   resources :users
 
   resources :account_activations, only: [:edit]
+  resources :password_resets,     only: [:new, :create, :edit, :update]
 
   resources :cheatsheets do
     resources :cheats
