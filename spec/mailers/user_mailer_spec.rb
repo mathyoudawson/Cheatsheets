425eb383d578167153dd3913d61e9c974e8bfe56
Matthew D <matthewdawson@outlook.com>
2018-12-27 22:59:45 +1300

    Generate new action mailer for password reset with link

--- spec/mailers/user_mailer_spec.rb
+++ spec/mailers/user_mailer_spec.rb
@@ -21,16 +21,21 @@ RSpec.describe UserMailer, type: :mailer do
   end
 
   describe "password_reset" do
-    let(:mail) { UserMailer.password_reset }
+    let(:user) { create(:user) }
+    let(:mail) { UserMailer.password_reset(user) }
 
     it "renders the headers" do
+      user.reset_token = User.new_token
       expect(mail.subject).to eq("Password reset")
-      expect(mail.to).to eq(["to@example.org"])
+      expect(mail.to).to eq([user.email])
       expect(mail.from).to eq(["from@example.com"])
-    end
+      expect(mail.body.encoded).to match("To reset your password click the link below:")
+      expect(mail.body.encoded).to match(user.reset_token)
+      expect(mail.body.encoded).to match(CGI.escape(user.email))
+          end
 
     it "renders the body" do
-      expect(mail.body.encoded).to match("Hi")
+      # For some unknown reason mail had an evaluation error here but didn't in the activation spec. Unsure why this happens. Would prefer to seperate but can't
     end
   end
 
