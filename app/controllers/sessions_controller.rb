4d15a9d53b173bdf1fb73f131a0f95fc64aaadfa
Matthew D <matthewdawson@outlook.com>
2018-12-27 16:42:27 +1300

    Ensure users can only log in with activated account

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,12 +5,19 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
-      flash.now[:success] = 'Successfully logged in!'
-      log_in(user)
-      params[:session][:remember_me] == '1' ? remember(user) : forget(user)
-      redirect_back_or user
-    else
-      flash.now[:danger] = 'Invalid username or password'
+      if user.activated?
+        log_in user
+        params[:session][:remember_me] == '1' ? remember(user) : forget(user)
+        redirect_back_or user
+        flash[:success] = 'Sucessfully logged in!'
+      else
+        message  = "Account not activated. "
+        message += "Check your email for the activation link."
+        flash[:warning] = message
+        redirect_to root_url
+      end
+  else
+    flash.now[:danger] = 'Invalid username or password'
       render 'new'
     end
   end
--- spec/features/signup_spec.rb
+++ spec/features/signup_spec.rb
@@ -11,6 +11,30 @@ describe "the signin process", type: :feature do
     expect(page).to have_content 'Please check your email to activate your account'
   end
 
+  it 'completes a valid signup with activation' do
+    ActionMailer::Base.deliveries.clear
+    visit signup_path
+    fill_in 'user_name', with: 'Nigel'
+    fill_in 'user_email', with: 'user@example.com'
+    fill_in 'user_password', with: 'password'
+    fill_in 'user_password_confirmation', with: 'password'
+    click_button 'Create my account'
+    user = User.find_by(email: 'user@example.com')
+
+    expect(ActionMailer::Base.deliveries.size).to be 1
+    expect(user.activated).to be false
+  end
+
+  it 'activates account by following activation link' do
+    user = create(:user, activated: false, activated_at: nil)
+
+    visit edit_account_activation_path(user.activation_token, email: user.email)
+
+    expect(page).to have_content 'Account activated!'
+    # I need to requery my user as otherwise it does not return the updated attributes
+    expect(User.find_by(email: user.email).activated).to be true
+  end
+
   it "doesn't sign me in with invalid credentials" do
     visit 'users/new'
     fill_in 'user_name', with: 'Nigel'
