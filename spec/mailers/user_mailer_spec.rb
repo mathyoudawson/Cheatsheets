ca25eb79519f772b00cba2ff3bf8b4aa69facef8
Matthew D <matthewdawson@outlook.com>
2018-12-27 13:01:54 +1300

    Generate mail for activating user

--- spec/mailers/user_mailer_spec.rb
+++ spec/mailers/user_mailer_spec.rb
@@ -2,16 +2,21 @@ require "rails_helper"
 
 RSpec.describe UserMailer, type: :mailer do
   describe "account_activation" do
-    let(:mail) { UserMailer.account_activation }
+    let(:user) { create(:user) }
+    let(:mail) { UserMailer.account_activation(user) }
 
     it "renders the headers" do
-      expect(mail.subject).to eq("Account activation")
-      expect(mail.to).to eq(["to@example.org"])
+      user.activation_token = User.new_token
+      expect(mail.subject).to eq("Account Activation")
+      expect(mail.to).to eq([user.email])
       expect(mail.from).to eq(["from@example.com"])
     end
 
     it "renders the body" do
-      expect(mail.body.encoded).to match("Hi")
+      expect(mail.body.encoded).to match("Welcome to Cheatsheets! Click on the link below to activate your account:")
+      expect(mail.body.encoded).to match(user.name)
+      expect(mail.body.encoded).to match(user.activation_token)
+      expect(mail.body.encoded).to match(CGI.escape(user.email))
     end
   end
 
