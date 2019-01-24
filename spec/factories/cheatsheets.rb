6c8e7d9a03da2510379edba2754e432f2c5e1e91
Matthew D <matthewdawson@outlook.com>
2019-01-24 21:36:09 +1300

    Create factory for cheatsheet with many cheats

--- spec/factories/cheatsheets.rb
+++ spec/factories/cheatsheets.rb
@@ -1,6 +1,23 @@
 FactoryBot.define do
   factory :cheatsheet do
-    title { 'Vim Cheatsheet' }
-    description  { 'A cheatsheet for vim' }
+    title { 'Test Cheatsheet' }
+    description  { 'A test cheatsheet' }
+
+    factory :cheatsheet_with_cheats do
+      transient do
+        cheats_count { 5 }
+      end
+
+      after(:create) do |cheatsheet, evaluator|
+        create_list(:cheat, evaluator.cheats_count, cheatsheet: cheatsheet)
+      end
+    end
+  end
+
+  factory :cheat do
+    sequence(:term){ |n| "Term#{n}" }
+    sequence(:category){ |n| "Category#{n}" }
+    sequence(:description){ |n| "Description#{n}" }
+    association :cheatsheet, factory: :cheatsheet, first_name: "Jon"
   end
 end
