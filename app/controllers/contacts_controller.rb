bcdc5e5c8469eb6d9d646100ac55cfa1a16941cd
Matthew D <matthewdawson@outlook.com>
2019-01-27 20:28:42 +1300

    Add ability to send email with contact

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,7 +1,27 @@
 class ContactsController < ApplicationController
   def new
+    @contact = Contact.new
   end
 
   def create
+    @contact = Contact.new(contact_params)
+
+    if @contact.save
+      ContactsMailer.general_message(@contact).deliver
+      redirect_to root_path
+      flash[:success] = 'Email successfully delivered'
+    else
+      render :new
+    end
+  end
+
+  def thanks
+    flash[:success] = 'Email successfully delivered'
+  end
+
+  private
+
+  def contact_params
+    params.require(:contact).permit(:name, :email, :message)
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_12_27_091401) do
+ActiveRecord::Schema.define(version: 2019_01_27_062811) do
 
   create_table "cheats", force: :cascade do |t|
     t.string "term"
@@ -29,6 +29,14 @@ ActiveRecord::Schema.define(version: 2018_12_27_091401) do
     t.string "description"
     t.integer "user_id"
     t.index ["user_id"], name: "index_cheatsheets_on_user_id"
+  end
+
+  create_table "contacts", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.text "message"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
